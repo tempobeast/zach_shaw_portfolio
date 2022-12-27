af2bd3554cb97037ea380e7b48c7ddcf81471491
Zach <shaw.zacht@gmail.com>
2022-12-26 23:04:10 -0800

    updated seed data

--- src/data.js
+++ src/data.js
@@ -3,17 +3,26 @@ const projectData = [
         name: "The Library of Toys",
         description: "Digital interface for a subscription based toy library with distinct user and admin views. Users can browse and add toys to their cart, order and track order status. Administrators can add and delete toys, and update order status.",
         image: "",
-        video: "https://youtu.be/0d6rHnPyPCo",
-        gitHub: "https://github.com/tempobeast/toy-library"
+        video: "https://www.youtube.com/embed/0d6rHnPyPCo",
+        gitHub: "https://github.com/tempobeast/toy-library",
+        url: "https://toy-library.herokuapp.com/"
     },
     {
         name: "The Cut List",
-        description: "",
+        description: "Woodworking social network app where users can upload completed projects, photos and design instructions for others to view, follow and try themselves.",
+        image: "",
+        video: "https://www.youtube.com/embed/8d6Ck09m2do",
+        gitHub: "https://github.com/tempobeast/phase-4-project-woodworking-app",
+        url: ""
+    },
+    {
+        name: "Track Tracker App",
+        description: "Workout scheduling and tracking for coaches. Training log for athletes.",
         image: "",
         video: "",
-        gitHub: ""
+        gitHub: "",
+        url: ""
     }
-
 ]
 
 export default projectData
