09b8be407416c49dc5a38f32a76fcd3d04896b90
Zach <shaw.zacht@gmail.com>
2022-12-26 22:58:01 -0800

    add app url and GitHub link to project card

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,12 +2,16 @@ import React from "react"
 import YouTubeEmbed from './YouTubeEmbed'
 
 function Project ({clickedProject}) {
-    const {name, description, video, gitHub, image} = clickedProject
+    const {name, description, video, gitHub, image, url} = clickedProject
 
     return (
         <div className="project">
-            <h3>{name}</h3>
-            <YouTubeEmbed name={name} video={video}></YouTubeEmbed>
+            <div className="project_titles_container">
+                <h3>{name}</h3>
+                {url ? <a href={url} className="social_links">App</a> : null}
+                {gitHub ? <a href={gitHub} className="social_links">GitHub</a> : null}
+            </div>
+            { clickedProject ? <YouTubeEmbed name={name} video={video}></YouTubeEmbed> : null}
             <p>{description}</p>
         </div>
     )
