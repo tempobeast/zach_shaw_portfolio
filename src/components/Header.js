4410bb9851b523ad474b8596def8f3edf77cdfe3
Zach <shaw.zacht@gmail.com>
2022-12-26 22:56:42 -0800

    add social media links to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,8 @@ function Header () {
         <div className="header">
             <h1>Zach Shaw</h1>
             <h4>Software Developer</h4>
+            <a href="https://github.com/tempobeast/" className="social_links">GitHub</a>
+            <a href="https://www.linkedin.com/in/shaw-zach/" className="social_links">LinkedIn</a>
         </div>
     )
 }
