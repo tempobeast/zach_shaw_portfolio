fe2c016e6e21268da347083cab641913f59fc480
Zach <shaw.zacht@gmail.com>
2023-02-08 15:18:48 -0800

    add classNames to name and title

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import React from "react"
 function Header () {
     return (
         <div className="header">
-            <h1>Zach Shaw</h1>
-            <h4>Software Developer</h4>
-            <a href="https://github.com/tempobeast/" className="social_links">GitHub</a>
-            <a href="https://www.linkedin.com/in/shaw-zach/" className="social_links">LinkedIn</a>
+            <h1 className="name">Zach Shaw</h1>
+            <h4 className="title">Full Stack Engineer</h4>
+            <a href="https://github.com/tempobeast/" className="social_links header-item">GitHub</a>
+            <a href="https://www.linkedin.com/in/shaw-zach/" className="social_links header-item">LinkedIn</a>
         </div>
     )
 }
